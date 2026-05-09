ad0495791c63ae390d82a7c16720264f8f08c138
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-05-09 23:57:27 +0800

    1.5.7

--- src/main/java/com/xiaoyue/celestial_core/content/series/package-info.java
+++
@@ -1,8 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-
-package com.xiaoyue.celestial_core.content.series;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
