3119c8dd9662ae64e9ecd3a24e89b432f533f287
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-08 19:25:12 +0800

    1.5.8

--- src/main/java/com/xiaoyue/celestial_core/content/generic/EntityIntData.java
+++ src/main/java/com/xiaoyue/celestial_core/content/generic/EntityIntData.java
@@ -15,53 +15,53 @@ import java.util.Map;
 @SerialClass
 public class EntityIntData extends GeneralCapabilityTemplate<LivingEntity, EntityIntData> {
 
-    public static void syncData(LivingEntity entity, String flag, int data) {
-        if (entity.level().isClientSide()) return;
-        CelestialCore.HANDLER.toTrackingPlayers(new EntityIntDataSyncPacket(entity.getId(), flag, data), entity);
-    }
+	public static void syncData(LivingEntity entity, String flag, int data) {
+		if (entity.level().isClientSide()) return;
+		CelestialCore.HANDLER.toTrackingPlayers(new EntityIntDataSyncPacket(entity.getId(), flag, data), entity);
+	}
 
-    public static void addData(LivingEntity entity, String key, int value) {
-        if (!entity.isDeadOrDying()) {
-            entity.getCapability(CAPABILITY).resolve().ifPresent(e -> e.addData(key, value));
-        }
-    }
+	public static void addData(LivingEntity entity, String key, int value) {
+		if (!entity.isDeadOrDying()) {
+			entity.getCapability(CAPABILITY).resolve().ifPresent(e -> e.addData(key, value));
+		}
+	}
 
-    public static int getData(LivingEntity entity, String key) {
-        if (!entity.isDeadOrDying()) {
-            return entity.getCapability(CAPABILITY).resolve().map(e -> e.getData(key)).orElse(0);
-        }
-        return 0;
-    }
+	public static int getData(LivingEntity entity, String key) {
+		if (!entity.isDeadOrDying()) {
+			return entity.getCapability(CAPABILITY).resolve().map(e -> e.getData(key)).orElse(0);
+		}
+		return 0;
+	}
 
-    public static void removeData(LivingEntity entity, String key) {
-        entity.getCapability(CAPABILITY).resolve().ifPresent(e -> e.removeData(key));
-        syncData(entity, key, 0);
-    }
+	public static void removeData(LivingEntity entity, String key) {
+		entity.getCapability(CAPABILITY).resolve().ifPresent(e -> e.removeData(key));
+		syncData(entity, key, 0);
+	}
 
-    public static final Capability<EntityIntData> CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
-    });
+	public static final Capability<EntityIntData> CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
+	});
 
-    public static final GeneralCapabilityHolder<LivingEntity, EntityIntData> HOLDER =
-            new GeneralCapabilityHolder<>(CelestialCore.loc("int_data"), CAPABILITY, EntityIntData.class,
-                    EntityIntData::new, LivingEntity.class, LivingEntity::isAlive);
+	public static final GeneralCapabilityHolder<LivingEntity, EntityIntData> HOLDER =
+			new GeneralCapabilityHolder<>(CelestialCore.loc("int_data"), CAPABILITY, EntityIntData.class,
+					EntityIntData::new, LivingEntity.class, e -> true);
 
-    @SerialClass.SerialField
-    private final Map<String, Integer> data = new HashMap<>();
+	@SerialClass.SerialField
+	private final Map<String, Integer> data = new HashMap<>();
 
-    public void addData(String key, int value) {
-        data.put(key, value);
-    }
+	public void addData(String key, int value) {
+		data.put(key, value);
+	}
 
-    public int getData(String key) {
-        return data.get(key) == null ? 0 : data.get(key);
-    }
+	public int getData(String key) {
+		return data.get(key) == null ? 0 : data.get(key);
+	}
 
-    public void removeData(String key) {
-        data.remove(key);
-    }
+	public void removeData(String key) {
+		data.remove(key);
+	}
 
-    public static void register() {
+	public static void register() {
 
-    }
+	}
 
 }
